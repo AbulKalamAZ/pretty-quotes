8ac930721a6e90a8c09f29a0a99f1d6f7b552cf9
ak_azad <ak_azad@solsersoft.com>
2019-07-31 05:51:07 +0600

    Updates

---
+++ precache-manifest.0268c078ad1a5ae95a4c9564a913c344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593dd73f52323efc6612a9966bb7cfca",
+    "url": "/pretty-quotes/index.html"
+  },
+  {
+    "revision": "99eb58809d4f28f745b5",
+    "url": "/pretty-quotes/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "d03f369ce9250bdb8364",
+    "url": "/pretty-quotes/static/css/main.65c9ec8c.chunk.css"
+  },
+  {
+    "revision": "99eb58809d4f28f745b5",
+    "url": "/pretty-quotes/static/js/2.18b723d1.chunk.js"
+  },
+  {
+    "revision": "d03f369ce9250bdb8364",
+    "url": "/pretty-quotes/static/js/main.4d57ade9.chunk.js"
+  },
+  {
+    "revision": "4dbefb53b75fb7da77aa",
+    "url": "/pretty-quotes/static/js/runtime~main.b323117f.js"
+  }
+]);
